5ad993f8810be3a3033a4c392b9174667535933a
Eekshitha-04 <eekshithavanampally@gmail.com>
2025-09-08 12:58:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+export const firebaseConfigReview = {
+    apiKey: "AIzaSyCiDqFQ0YKsbHUy4QS5NRzY0C0tJ2pirzs",
+    authDomain: "shaktipeethareviews.firebaseapp.com",
+    databaseURL: "https://shaktipeethareviews-default-rtdb.firebaseio.com",
+    projectId: "shaktipeethareviews",
+    storageBucket: "shaktipeethareviews.firebasestorage.app",
+    messagingSenderId: "815409406003",
+    appId: "1:815409406003:web:82485d40ba6a91260c0346"
+  };
+export const GEMINI_API_KEY = "AIzaSyAuv5e9GRI6VHzd1bUETHiRh18g4-bVyyk";
+export const firebaseConfig = {
+    apiKey: "AIzaSyBnsyvPC17GvBwAqylYWlbVWRvh3t8okq8",
+    authDomain: "shakthi-peethas.firebaseapp.com",
+    projectId: "shakthi-peethas",
+    storageBucket: "shakthi-peethas.appspot.com",
+    messagingSenderId: "910792313415",
+    appId: "1:910792313415:web:39448c248cbb745851ff62"
+  };
+export const apiKey = 'a20b221e9ef5baed6d9d0d37f7db90df';
